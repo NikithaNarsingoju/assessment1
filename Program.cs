2bd318e38caaded1e8a58839ed7167311223d2cc
HP <HP@NIKITHA-NARSING>
2023-10-05 15:03:47 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assessment1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // SwapTwoNos();
+            multiply();
+            Console.ReadLine();
+
+
+        }
+        private static void SwapTwoNos()
+        {
+            Console.WriteLine("input the first number:");
+            int Firstno = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("input the second number:");
+            int Secondno = Convert.ToInt32(Console.ReadLine());
+            int temp = Firstno;
+            Firstno = Secondno;
+            Secondno = temp;
+            Console.WriteLine("After Swapping");
+            Console.WriteLine($"First number : {Firstno}");
+            Console.WriteLine($"Secong number : {Secondno}");
+        }
+        private static void multiply()
+        {
+            int Firstno;
+            int Secondno;
+            int Thirdno;
+            Console.WriteLine("input the first number to multiply:");
+            Firstno = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("input the second number to multiply:");
+            Secondno = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("input the third number to multiply:");
+            Thirdno = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"{Firstno}*{Secondno}*{Thirdno}={Firstno * Secondno * Thirdno}");
+        }
+    }
+   
+}
+
